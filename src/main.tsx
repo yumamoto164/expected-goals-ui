6fea14c81605e86c79f9e95dbf044567c7f0fd02
Yuma Yamamoto <yumamoto164@gmail.com>
2026-03-01 09:58:56 -0500

    feat: add vercel analytics to project

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import './index.css'
 import App from './App.tsx'
 import { GameProvider } from './context/GameContext.tsx'
@@ -9,5 +10,6 @@ createRoot(document.getElementById('root')!).render(
     <GameProvider>
       <App />
     </GameProvider>
+    <Analytics />
   </StrictMode>,
 )
